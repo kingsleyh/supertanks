daf47998484ffcbbff5493c519fe6ba4597d30bb
Kingsley Hendrickse <Kingsley.Hendrickse@barclayscapital.com>
2015-03-24 18:33:53 +0000

    added initial tank

---
+++ tanks/staticbot.d
@@ -0,0 +1,34 @@
+module net.masterthought.dtanks.samples.staticbot;
+
+import net.masterthought.dtanks.brainhelper;
+import net.masterthought.dtanks.bot.brain;
+import net.masterthought.dtanks.arena;
+import net.masterthought.dtanks.bot.sensor;
+import net.masterthought.dtanks.bot.command;
+import net.masterthought.dtanks.heading;
+import net.masterthought.dtanks.skincolor;
+import dsfml.graphics;
+
+import std.random;
+import std.stdio;
+import std.range;
+
+class StaticBot : Brain {
+
+   static this(){
+     Brain.add(new StaticBot());
+   }
+
+  override public Command tick(Sensor sensors){
+    return command;
+  }
+
+  override public string name(){
+    return "static_tank";
+  }
+
+  override public SkinColor skin(){
+    return SkinColor.green;
+  }
+
+}
